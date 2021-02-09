69ab9a72327c4ebec01a07329be41c4a6c30a932
St-Dem <dmd.stanislav@gmail.com>
2021-02-09 16:46:01 +0300

    HW1 before HW

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -4,29 +4,14 @@ import java.time.LocalDateTime;
 
 public class MealTo {
     private final LocalDateTime dateTime;
-
     private final String description;
-
     private final int calories;
-
-//    private final AtomicBoolean excess;      // or Boolean[1],  filteredByAtomic
-//    private final Boolean excess;            // filteredByReflection
-//    private final Supplier<Boolean> excess;  // filteredByClosure
     private boolean excess;
 
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
-        this.excess = excess;
-    }
-
-//    public Boolean getExcess() {
-//        return excess.get();
-//    }
-
-    // for filteredBySetterRecursion
-    public void setExcess(boolean excess) {
         this.excess = excess;
     }
 
